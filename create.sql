725ce8f46963715f46b8f6a56c82a2745b3c3ae9
akasinan <akasinan@g.ucla.edu>
2019-01-26 20:48:42 -0800

    Add files via upload

--- create.sql
+++ create.sql
@@ -1,5 +1,5 @@
 CREATE TABLE Movie (
-  id INT PRIMARY KEY,
+  id INT PRIMARY KEY, --Every movie has an unique ID number
   title VARCHAR(100),
   year INT,
   rating VARCHAR(10),
@@ -7,7 +7,7 @@ CREATE TABLE Movie (
 )ENGINE = INNODB;
 
 CREATE TABLE Actor (
-  id INT PRIMARY KEY,
+  id INT PRIMARY KEY, --Every actor has an unique ID number
   last VARCHAR(20),
   first VARCHAR(20),
   sex VARCHAR(6),
@@ -16,13 +16,13 @@ CREATE TABLE Actor (
 )ENGINE = INNODB;
 
 CREATE TABLE Sales (
-  mid INT FOREIGN KEY,
+  mid INT FOREIGN KEY REFERENCES Movie(id), --
   ticketsSold INT,
   totalIncome INT
 )ENGINE = INNODB;
 
 CREATE TABLE Director (
-  id INT PRIMARY KEY,
+  id INT PRIMARY KEY, --Every director has an unique ID number
   last VARCHAR(20),
   first VARCHAR(20),
   dob DATE,
@@ -35,18 +35,18 @@ CREATE TABLE MovieGenre (
 )ENGINE = INNODB;
 
 CREATE TABLE MovieDirector (
-  mid INT FOREIGN KEY,
-  did INT FOREIGN KEY
+  mid INT FOREIGN KEY REFERENCES Movie(id),
+  did INT FOREIGN KEY REFERENCES Director(id)
 )ENGINE = INNODB;
 
 CREATE TABLE MovieActor (
-  mid INT FOREIGN KEY,
-  aid INT FOREIGN KEY,
+  mid INT FOREIGN KEY REFERENCES Movie(id),
+  aid INT FOREIGN KEY REFERENCES Actor(id),
   role VARCHAR(50)
 )ENGINE = INNODB;
 
 CREATE TABLE MovieRating (
-  mid INT FOREIGN KEY,
+  mid INT FOREIGN KEY REFERENCES Movie(id),
   imdb INT,
   rot INT
 )ENGINE = INNODB;
@@ -54,7 +54,7 @@ CREATE TABLE MovieRating (
 CREATE TABLE Review (
   name VARCHAR(20),
   time TIMESTAMP,
-  mid INT FOREIGN KEY,
+  mid INT FOREIGN KEY REFERENCES Movie(id),
   rating INT,
   comment VARCHAR(500)
 )ENGINE = INNODB;
