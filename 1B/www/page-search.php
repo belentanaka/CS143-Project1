40d05ea5d422ce6017ee69eb4202027fdf72c35e
takashi <brendantanaka@gmail.com>
2019-02-10 02:20:43 -0800

    Implemented search

--- 1B/www/page-search.php
+++ 1B/www/page-search.php
@@ -1,142 +1,103 @@
-<<<<<<< HEAD
+<div class="content">
+
 <h2>Search for an Actor or Movie</h2>
 
 <form method="GET">
   <input type="hidden" name="page" value="search">
 
-  <p><b>Search for an actor </b><br />
+  <p><b>Enter a search:</b><br />
   <INPUT TYPE="text" NAME="search" SIZE=50 >
   </p>
 
   <input type="submit" value="Submit" />
 </form>
 
-<p><b>Actor Results</b><br />
-<?php
-  $db_connection = mysql_connect("localhost", "cs143", "");
-  mysql_select_db("CS143", $db_connection);
-  $search = $_GET["search"];
-  $query = "SELECT * FROM Actor WHERE first = ";
-  $searchlist = explode(" ", $search);
-  foreach($searchlist as $s){
-	  $query = "SELECT * FROM Actor WHERE first = ";
-	  $query = $query.$s;
-	  $query = $query.";";
-	  echo $query;
-	  $rs = mysql_query($query, $db_connection);
-  }
-  print "<table border='1'>";
-  for($i = 0; $i < mysql_num_fields($rs); $i++) {
-    $field_info = mysql_fetch_field($rs, $i);
-    print "<th>{$field_info->name}</th>";
-  }
-  while($row = mysql_fetch_row($rs)) {
-    print "<tr>";
-    foreach($row as $value) {
-      print "<td>$value</td>";
-    }
-    print "</tr>";
-  }
-  print "</table>";
-?>
-</p>
-
-<hr>
-
-<p><b>Movie Results</b><br />
 <?php
   $search = $_GET["search"];
-  $query = "SELECT * FROM Movie WHERE title = ";
-  $query = $query.$search;
-  $query = $query.";";
-  echo $query;
-  $rs = mysql_query($query, $db_connection);
-  echo $rs;
-  print "<table border='1'>";
-  for($i = 0; $i < mysql_num_fields($rs); $i++) {
-    $field_info = mysql_fetch_field($rs, $i);
-    print "<th>{$field_info->name}</th>";
-  }
-  while($row = mysql_fetch_row($rs)) {
-    print "<tr>";
-    foreach($row as $value) {
-      print "<td>$value</td>";
+  if(isset($search)) {
+    $db_connection = mysql_connect("localhost", "cs143", "");
+    mysql_select_db("CS143", $db_connection);
+
+    $words = explode(' ', $search);
+
+    print "<p><b>Actor Results</b><br />";
+
+    $query = "SELECT id, CONCAT(first, ' ', last) AS name, dob, dod FROM Actor WHERE ";
+
+    for($i = 0; $i < count($words); $i++) {
+      $word = $words[$i];
+      $query .= "(first LIKE '%$word%' OR last LIKE '%$word%')";
+      if($i + 1 != count($words)) {
+        $query .= " AND ";
+      }
     }
-    print "</tr>";
-  }
-  print "</table>";
 
-  mysql_close($db_connection);
-?>
-</p>
-=======
-<h2>Search for an Actor or Movie</h2>
-
-<form method="GET">
-  <input type="hidden" name="page" value="search">
-
-  <p><b>Search for an actor </b><br />
-  <INPUT TYPE="text" NAME="search" SIZE=50 >
-  </p>
-
-  <input type="submit" value="Submit" />
-</form>
-
-<p><b>Actor Results</b><br />
-<?php
-  $db_connection = mysql_connect("localhost", "cs143", "");
-  mysql_select_db("CS143", $db_connection);
-  $search = $_GET["search"];
-  $query = "SELECT * FROM Actor WHERE first = ";
-  $searchlist = explode(" ", $search);
-  foreach($searchlist as $s){
-	  $query = "SELECT * FROM Actor WHERE first = ";
-	  $query = $query.$s; 
-	  $query = $query.";";
-	  echo $query;
-	  $rs = mysql_query($query, $db_connection);
-  }
-  print "<table border='1'>";
-  for($i = 0; $i < mysql_num_fields($rs); $i++) {
-    $field_info = mysql_fetch_field($rs, $i);
-    print "<th>{$field_info->name}</th>";
-  }
-  while($row = mysql_fetch_row($rs)) {
-    print "<tr>";
-    foreach($row as $value) {
-      print "<td>$value</td>";
+    $rs = mysql_query($query, $db_connection);
+    print "<table border='1'>";
+    for($i = 0; $i < mysql_num_fields($rs); $i++) {
+      $field_info = mysql_fetch_field($rs, $i);
+      print "<th>{$field_info->name}</th>";
     }
-    print "</tr>";
-  }
-  print "</table>";
-?>
-</p>
-
-<hr>
-
-<p><b>Movie Results</b><br />
-<?php
-  $search = $_GET["search"];
-  $query = "SELECT * FROM Movie WHERE title = ";
-  $query = $query.$search; 
-  $query = $query.";";
-  echo $query;
-  $rs = mysql_query($query, $db_connection);
-  print "<table border='1'>";
-  for($i = 0; $i < mysql_num_fields($rs); $i++) {
-    $field_info = mysql_fetch_field($rs, $i);
-    print "<th>{$field_info->name}</th>";
-  }
-  while($row = mysql_fetch_row($rs)) {
-    print "<tr>";
-    foreach($row as $value) {
-      print "<td>$value</td>";
+    while($row = mysql_fetch_row($rs)) {
+      print "<tr>";
+      $first = 1;
+      foreach($row as $value) {
+        if($first) {
+          print "<td><a href='index.php?page=browse-actor?id=$value'>$value</a></td>";
+          $first = 0;
+        }
+        else if(is_null($value)) {
+          print "<td>NULL</td>";
+        }
+        else {
+          print "<td>$value</td>";
+        }
+      }
+      print "</tr>";
     }
-    print "</tr>";
+    print "</table>";
+
+    print "<hr>";
+
+    print "<p><b>Movie Results</b><br />";
+
+    $query = "SELECT id, title, year FROM Movie WHERE ";
+
+    for($i = 0; $i < count($words); $i++) {
+      $word = $words[$i];
+      $query .= "title LIKE '%$word%'";
+      if($i + 1 != count($words)) {
+        $query .= " AND ";
+      }
+    }
+
+    $rs = mysql_query($query, $db_connection);
+    print "<table border='1'>";
+    for($i = 0; $i < mysql_num_fields($rs); $i++) {
+      $field_info = mysql_fetch_field($rs, $i);
+      print "<th>{$field_info->name}</th>";
+    }
+    while($row = mysql_fetch_row($rs)) {
+      print "<tr>";
+      $first = 1;
+      foreach($row as $value) {
+        if($first) {
+          print "<td><a href='index.php?page=browse-movie?id=$value'>$value</a></td>";
+          $first = 0;
+        }
+        else if(is_null($value)) {
+          print "<td>NULL</td>";
+        }
+        else {
+          print "<td>$value</td>";
+        }
+      }
+      print "</tr>";
+    }
+    print "</table>";
+
+    mysql_close($db_connection);
   }
-  print "</table>";
-  
-  mysql_close($db_connection);
 ?>
-</p>
->>>>>>> 16a5b67981689daf2365afe25dbc2b6adcc65d5c
+
+</div>
