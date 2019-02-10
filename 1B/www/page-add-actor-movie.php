6d5d5b5d45fe66260ad7e5cb32dda69f8e99a5ad
takashi <brendantanaka@gmail.com>
2019-02-10 00:01:38 -0800

    Implemented add-actor-movie

--- 1B/www/page-add-actor-movie.php
+++ 1B/www/page-add-actor-movie.php
@@ -1,15 +1,40 @@
+<div class="content">
+
 <h2>Add Actor to Movie Cast</h2>
 
 <form method="GET">
-  <input type="hidden" name="page" value="add-comment">
+  <input type="hidden" name="page" value="add-actor-movie">
 
-  <p><b>Actor ID</b><br />
-  <INPUT TYPE="text" NAME="aid" SIZE=10 >
-  </p>
-  
-  <p><b>Movie ID</b><br />
-  <INPUT TYPE="text" NAME="mid" SIZE=10 >
-  </p>
+  <p><b>Actor</b><br />
+  <select name="aid">
+    <?php
+      $db_connection = mysql_connect("localhost", "cs143", "");
+      mysql_select_db("CS143", $db_connection);
+      $rs = mysql_query("SELECT id, first, last FROM Actor");
+      while($row = mysql_fetch_row($rs)) {
+        $last = array_pop($row);
+        $first = array_pop($row);
+        $id = array_pop($row);
+        print "<option value='$id'>$first $last";
+      }
+      mysql_close($db_connection);
+    ?>
+  </select>
+
+  <p><b>Movie</b><br />
+  <select name="mid">
+    <?php
+      $db_connection = mysql_connect("localhost", "cs143", "");
+      mysql_select_db("CS143", $db_connection);
+      $rs = mysql_query("SELECT id, title FROM Movie");
+      while($row = mysql_fetch_row($rs)) {
+        $title = array_pop($row);
+        $id = array_pop($row);
+        print "<option value='$id'>$title";
+      }
+      mysql_close($db_connection);
+    ?>
+  </select>
 
   <p><b>Role</b><br />
   <INPUT TYPE="text" NAME="role" SIZE=30 MAXLENGTH=50>
@@ -19,12 +44,27 @@
 </form>
 
 <?php
-  $db_connection = mysql_connect("localhost", "cs143", "");
-  mysql_select_db("CS143", $db_connection);
   $aid = $_GET["aid"];
-  $mid = $_GET["mid"];
-  $role = $_GET["role"];
-  $query;
-  $rs = mysql_query($query, $db_connection);
-  mysql_close($db_connection);
+  if(isset($aid)) {
+    $db_connection = mysql_connect("localhost", "cs143", "");
+    mysql_select_db("CS143", $db_connection);
+
+    $mid = $_GET["mid"];
+    $role = $_GET["role"];
+
+    $query = "INSERT INTO MovieActor VALUES($mid, $aid, '$role')";
+
+    $rs = mysql_query($query, $db_connection);
+
+    if($rs) {
+      echo "Successfully Added Actor To Movie!";
+    }
+    else {
+      echo "ERROR: Failed To Add Actor To Movie";
+    }
+
+    mysql_close($db_connection);
+  }
 ?>
+
+</div>
