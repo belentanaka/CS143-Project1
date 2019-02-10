012bc6c0474493018e57accc5612520a831277d0
takashi <brendantanaka@gmail.com>
2019-02-09 23:43:12 -0800

    Implemented comments

--- 1B/sql/create.sql
+++ 1B/sql/create.sql
@@ -2,10 +2,10 @@
 -- CHK_Movieyear: There were no movies before 1888 (most likely)
 CREATE TABLE Movie (
   id INT PRIMARY KEY,
-  title VARCHAR(100),
-  year INT,
-  rating VARCHAR(10),
-  company VARCHAR(50),
+  title VARCHAR(100) NOT NULL,
+  year INT NOT NULL,
+  rating VARCHAR(10) NOT NULL,
+  company VARCHAR(50) NOT NULL,
   CONSTRAINT CHK_MovieYear CHECK (year>=1888)
 )ENGINE = INNODB;
 
@@ -13,43 +13,43 @@ CREATE TABLE Movie (
 -- CHK_Sex: Sex can only be Male or Female
 CREATE TABLE Actor (
   id INT PRIMARY KEY,
-  last VARCHAR(20),
-  first VARCHAR(20),
-  sex VARCHAR(6),
-  dob DATE,
+  last VARCHAR(20) NOT NULL,
+  first VARCHAR(20) NOT NULL,
+  sex VARCHAR(6) NOT NULL,
+  dob DATE NOT NULL,
   dod DATE,
   CONSTRAINT CHK_Sex CHECK (sex='Male' OR sex='Female' OR sex='Transgender')
 )ENGINE = INNODB;
 
 -- FOREIGN KEY (mid): mid refers to primary key in Movie
 CREATE TABLE Sales (
-  mid INT,
-  ticketsSold INT,
-  totalIncome INT,
+  mid INT NOT NULL,
+  ticketsSold INT NOT NULL,
+  totalIncome INT NOT NULL,
   FOREIGN KEY (mid) REFERENCES Movie(id)
 )ENGINE = INNODB;
 
 -- PRIMARY KEY: Every director has a unique ID
 CREATE TABLE Director (
   id INT PRIMARY KEY,
-  last VARCHAR(20),
-  first VARCHAR(20),
-  dob DATE,
+  last VARCHAR(20) NOT NULL,
+  first VARCHAR(20) NOT NULL,
+  dob DATE NOT NULL,
   dod DATE
 )ENGINE = INNODB;
 
 -- FOREIGN KEY (mid): mid refers to primary key in Movie
 CREATE TABLE MovieGenre (
-  mid INT,
-  genre VARCHAR(20),
+  mid INT NOT NULL,
+  genre VARCHAR(20) NOT NULL,
   FOREIGN KEY (mid) REFERENCES Movie(id)
 )ENGINE = INNODB;
 
 -- FOREIGN KEY (mid): mid refers to primary key in Movie
 -- FOREIGN KEY (did): mid refers to primary key in Director
 CREATE TABLE MovieDirector (
-  mid INT,
-  did INT,
+  mid INT NOT NULL,
+  did INT NOT NULL,
   FOREIGN KEY (mid) REFERENCES Movie(id),
   FOREIGN KEY (did) REFERENCES Director(id)
 )ENGINE = INNODB;
@@ -57,8 +57,8 @@ CREATE TABLE MovieDirector (
 -- FOREIGN KEY (mid): mid refers to primary key in Movie
 -- FOREIGN KEY (aid): mid refers to primary key in Actor
 CREATE TABLE MovieActor (
-  mid INT,
-  aid INT,
+  mid INT NOT NULL,
+  aid INT NOT NULL,
   role VARCHAR(50),
   FOREIGN KEY (mid) REFERENCES Movie(id),
   FOREIGN KEY (aid) REFERENCES Actor(id)
@@ -68,9 +68,9 @@ CREATE TABLE MovieActor (
 -- CHK_Imdb: IMDb scores can only be in the range 0 and 100
 -- CHK_Rot: Rotten Tomatoes scores can only be in the range 0 and 100
 CREATE TABLE MovieRating (
-  mid INT,
-  imdb INT,
-  rot INT,
+  mid INT NOT NULL,
+  imdb INT NOT NULL,
+  rot INT NOT NULL,
   FOREIGN KEY (mid) REFERENCES Movie(id),
   CONSTRAINT CHK_Imdb CHECK (imdb>=0 AND imdb<=100),
   CONSTRAINT CHK_Rot CHECK (rot>=0 AND rot<=100)
@@ -78,11 +78,11 @@ CREATE TABLE MovieRating (
 
 -- FOREIGN KEY (mid): mid refers to primary key in Movie
 CREATE TABLE Review (
-  name VARCHAR(20),
-  time TIMESTAMP,
-  mid INT,
-  rating INT,
-  comment VARCHAR(500),
+  name VARCHAR(20) NOT NULL,
+  time TIMESTAMP NOT NULL,
+  mid INT NOT NULL,
+  rating INT NOT NULL,
+  comment VARCHAR(500) NOT NULL,
   FOREIGN KEY (mid) REFERENCES Movie(id)
 )ENGINE = INNODB;
 
--- 1B/www/page-add-comment.php
+++ 1B/www/page-add-comment.php
@@ -1,8 +1,6 @@
-<!DOCTYPE html>
-<html>
-<body>
+<div class="content">
 
-<h2>Insert New Movie</h2>
+<h2>Insert New Comment</h2>
 
 <form method="GET">
   <input type="hidden" name="page" value="add-comment">
@@ -10,13 +8,24 @@
   <p><b>Reviewer Name</b><br />
   <INPUT TYPE="text" NAME="name" SIZE=20 MAXLENGTH=20>
   </p>
-  
-  <p><b>Movie ID</b><br />
-  <INPUT TYPE="text" NAME="mid" SIZE=10 >
-  </p>
+
+  <p><b>Movie</b><br />
+  <select name="mid">
+    <?php
+      $db_connection = mysql_connect("localhost", "cs143", "");
+      mysql_select_db("CS143", $db_connection);
+      $rs = mysql_query("SELECT id, title FROM Movie");
+      while($row = mysql_fetch_row($rs)) {
+        $title = array_pop($row);
+        $id = array_pop($row);
+        print "<option value='$id'>$title";
+      }
+      mysql_close($db_connection);
+    ?>
+  </select>
 
   <p><b>Rating</b><br />
-  <SELECT NAME="genre">
+  <SELECT NAME="rating">
   <OPTION>1
   <OPTION>2
   <OPTION>3
@@ -37,15 +46,33 @@
 </form>
 
 <?php
-  $db_connection = mysql_connect("localhost", "cs143", "");
-  mysql_select_db("CS143", $db_connection);
   $name = $_GET["name"];
-  $mid = $_GET["mid"];
-  $rating = $_GET["rating"];
-  $comment = $_GET["comment"];
-  $timestamp = "NOW()";
-  $query;
-  $rs = mysql_query($query, $db_connection);
-  mysql_close($db_connection);
+  if(isset($name)) {
+    $db_connection = mysql_connect("localhost", "cs143", "");
+    mysql_select_db("CS143", $db_connection);
+
+    $mid = $_GET["mid"];
+    $rating = $_GET["rating"];
+    $comment = $_GET["comment"];
+
+    $rs = mysql_query("SELECT NOW()", $db_connection);
+    while($row = mysql_fetch_row($rs)) {
+      $timestamp = array_pop($row);
+    }
+
+    $query = "INSERT INTO Review VALUES('$name', '$timestamp', $mid, $rating, '$comment')";
+
+    $rs = mysql_query($query, $db_connection);
+
+    if($rs) {
+      echo "Successfully Added Comment!";
+    }
+    else {
+      echo "ERROR: Failed To Add Comment";
+    }
+
+    mysql_close($db_connection);
+  }
 ?>
-</html>
+
+</div>
